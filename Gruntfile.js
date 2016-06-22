edaa81ccc4bf6e1e427ab86efb00389635bda2b8
Željko Filipin <zeljko.filipin@gmail.com>
2016-06-22 17:41:44 +0200

    the minimal test file
    grunt runs mocha

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Configure a mochaTest task 
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          timeout: 10000
+        },
+        src: ['test/**/*.js']
+      }
+    }
+  });
+
+  // Add the grunt-mocha-test tasks. 
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  // Default task(s).
+  grunt.registerTask('default', ['mochaTest']);
+
+};
---
+++ test/screenshot.js
@@ -0,0 +1,23 @@
+ var By = require('selenium-webdriver').By,
+     until = require('selenium-webdriver').until,
+     chrome = require('selenium-webdriver/chrome'),
+     test = require('selenium-webdriver/testing');
+
+test.describe('Google Search', function() {
+  var driver;
+
+  test.before(function() {
+    driver = new chrome.Driver();
+  });
+
+  test.after(function() {
+    driver.quit();
+  });
+
+  test.it('should append query to title', function() {
+    driver.get('http://www.google.com/ncr');
+    driver.findElement(By.name('q')).sendKeys('webdriver');
+    driver.findElement(By.name('btnG')).click();
+    driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  });
+});
