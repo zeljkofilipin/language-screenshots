b1bee7ffbe6b0cc37dc7615a538d0bb1705dc271
Ed Sanders <ejsanders@gmail.com>
2016-06-23 18:56:16 +0100

    Add JSCS config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,23 @@
-module.exports = function(grunt) {
+module.exports = function ( grunt ) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Configure a mochaTest task 
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec',
-          timeout: 10000
-        },
-        src: ['test/**/*.js']
-      }
-    }
-  });
+	// Project configuration.
+	grunt.initConfig( {
+		// Configure a mochaTest task
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec',
+					timeout: 10000
+				},
+				src: [ 'test/**/*.js' ]
+			}
+		}
+	} );
 
-  // Add the grunt-mocha-test tasks. 
-  grunt.loadNpmTasks('grunt-mocha-test');
+	// Add the grunt-mocha-test tasks.
+	grunt.loadNpmTasks( 'grunt-mocha-test' );
 
-  // Default task(s).
-  grunt.registerTask('default', ['mochaTest']);
+	// Default task(s).
+	grunt.registerTask( 'default', [ 'mochaTest' ] );
 
 };
--- test/screenshot.js
+++ test/screenshot.js
@@ -1,86 +1,90 @@
-var By = require('selenium-webdriver').By,
-    until = require('selenium-webdriver').until,
-    chrome = require('selenium-webdriver/chrome'),
-    test = require('selenium-webdriver/testing'),
-    fs = require('fs');
+var By = require( 'selenium-webdriver' ).By,
+	until = require( 'selenium-webdriver' ).until,
+	chrome = require( 'selenium-webdriver/chrome' ),
+	test = require( 'selenium-webdriver/testing' ),
+	fs = require( 'fs' ),
+	Jimp = require( 'jimp' );
 
-test.describe('Screenshot', function() {
-  var driver;
+test.describe( 'Screenshot', function () {
+	var driver;
 
-  test.beforeEach(function() {
-    driver = new chrome.Driver();
-  });
+	test.beforeEach( function () {
+		driver = new chrome.Driver();
+	} );
 
-  test.afterEach(function() {
-    driver.quit();
-  });
+	test.afterEach( function () {
+		driver.quit();
+	} );
 
-  test.it('should open visual editor without welcome dialogue', function() {
-    driver.get('http://en.wikipedia.beta.wmflabs.org/wiki/Special:Random?veaction=edit');
-    driver.manage().timeouts().setScriptTimeout(10000);
-    driver.wait(
-      driver.executeAsyncScript(
-        // This function is converted to a string and executed in the browser
-        function () {
-          var done = arguments[arguments.length-1]
-          mw.hook( 've.activationComplete' ).add( function() {
-            ve.init.target.welcomeDialog.close().then( done )
-          });
-        }
-      ).then(function() {
-        driver.takeScreenshot().then((image) => {
-          fs.writeFile('ve-without-welcome-dialogue.png', image, 'base64');
-        })
-      }), 10000
-    );
-  });
-  test.it('should open visual editor with welcome dialogue', function() {
-    driver.get('http://en.wikipedia.beta.wmflabs.org/wiki/Special:Random?veaction=edit');
-    driver.manage().timeouts().setScriptTimeout(10000);
-    driver.wait(
-      driver.executeAsyncScript(
-        // This function is converted to a string and executed in the browser
-        function () {
-          var done = arguments[arguments.length-1]
-          mw.hook( 've.activationComplete' ).add( function() {
-            ve.init.target.surface.dialogs.opening.then( done );
-          });
-        }
-      ).then(function() {
-        driver.takeScreenshot().then((image) => {
-          fs.writeFile('ve-with-welcome-dialogue.png', image, 'base64');
-        })
-      }), 10000
-    );
-  });
-  test.it('should open cite dialogue', function() {
-    driver.get('http://en.wikipedia.beta.wmflabs.org/wiki/Special:Random?vehidebetadialog=true&veaction=edit');
-    driver.manage().timeouts().setScriptTimeout(10000);
-    driver.wait(
-      driver.executeAsyncScript(
-        // This function is converted to a string and executed in the browser
-        function () {
-          var done = arguments[arguments.length-1]
-          mw.hook( 've.activationComplete' ).add( function() {
-            ve.init.target.surface.view.focus();
-            ve.init.target.toolbar.tools.citefromid.onSelect();
-            setTimeout(function() {
-              var rect = ve.init.target.surface.context.inspectors.currentWindow.$element[0].getBoundingClientRect();
-              done( rect );
-            }, 500);
-          });
-        }
-      ).then(function( rect ) {
-        driver.takeScreenshot().then((image) => {
-          fs.writeFileSync('ve-with-cite-dialogue-original.png', image, 'base64');
-          var Jimp = require("jimp");
-          Jimp.read( 've-with-cite-dialogue-original.png', function( err, jimpImage ) {
-            if ( err ) { console.log( err ) }
-            console.log(jimpImage);
-            jimpImage.crop( rect.left, rect.top, rect.width, rect.height ).write( "ve-with-cite-dialogue.png" )
-          } )
-        })
-      }), 10000
-    );
-  });
-});
+	test.it( 'should open visual editor without welcome dialogue', function () {
+		driver.get( 'http://en.wikipedia.beta.wmflabs.org/wiki/Special:Random?veaction=edit' );
+		driver.manage().timeouts().setScriptTimeout( 10000 );
+		driver.wait(
+			driver.executeAsyncScript(
+				// This function is converted to a string and executed in the browser
+				function () {
+					var done = arguments[ arguments.length - 1 ];
+					mw.hook( 've.activationComplete' ).add( function () {
+						ve.init.target.welcomeDialog.close().then( done );
+					} );
+				}
+			).then( function () {
+				driver.takeScreenshot().then( function ( image ) {
+					fs.writeFile( 've-without-welcome-dialogue.png', image, 'base64' );
+				} );
+			} ), 10000
+		);
+	} );
+	test.it( 'should open visual editor with welcome dialogue', function () {
+		driver.get( 'http://en.wikipedia.beta.wmflabs.org/wiki/Special:Random?veaction=edit' );
+		driver.manage().timeouts().setScriptTimeout( 10000 );
+		driver.wait(
+			driver.executeAsyncScript(
+				// This function is converted to a string and executed in the browser
+				function () {
+					var done = arguments[ arguments.length - 1 ];
+					mw.hook( 've.activationComplete' ).add( function () {
+						ve.init.target.surface.dialogs.opening.then( done );
+					} );
+				}
+			).then( function () {
+				driver.takeScreenshot().then( function ( image ) {
+					fs.writeFile( 've-with-welcome-dialogue.png', image, 'base64' );
+				} );
+			} ), 10000
+		);
+	} );
+	test.it( 'should open cite dialogue', function () {
+		driver.get( 'http://en.wikipedia.beta.wmflabs.org/wiki/Special:Random?vehidebetadialog=true&veaction=edit' );
+		driver.manage().timeouts().setScriptTimeout( 10000 );
+		driver.wait(
+			driver.executeAsyncScript(
+				// This function is converted to a string and executed in the browser
+				function () {
+					var done = arguments[ arguments.length - 1 ];
+					mw.hook( 've.activationComplete' ).add( function () {
+						ve.init.target.surface.view.focus();
+						ve.init.target.toolbar.tools.citefromid.onSelect();
+						setTimeout( function () {
+							var rect = ve.init.target.surface.context.inspectors.currentWindow.$element[ 0 ].getBoundingClientRect();
+							done( rect );
+						}, 500 );
+					} );
+				}
+			).then( function ( rect ) {
+				driver.takeScreenshot().then( function ( image ) {
+					fs.writeFileSync( 've-with-cite-dialogue-original.png', image, 'base64' );
+					Jimp.read( 've-with-cite-dialogue-original.png', function ( err, jimpImage ) {
+						if ( err ) {
+							console.log( err );
+						}
+						console.log( jimpImage );
+						jimpImage
+							.crop( rect.left, rect.top, rect.width, rect.height )
+							.write( 've-with-cite-dialogue.png' );
+					} );
+				} );
+			} ), 10000
+		);
+	} );
+} );
